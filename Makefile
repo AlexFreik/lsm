452f415e3b4b06e99652ae8859b4be9cf112611f
AlexFreik <alexfreik5@gmail.com>
2024-02-14 14:27:36 +0530

    added daisy ui

--- Makefile
+++ Makefile
@@ -8,3 +8,9 @@ pretty:
 
 start:
 	npm start
+
+css-gal:
+	npx tailwindcss -i gallery/input.css -o gallery/output.css --watch
+
+css-gal-ext:
+	npx tailwindcss -i gallery-ext/input.css -o gallery-ext/output.css --watch
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./gallery/**/*.{html,js}', './gallery-ext/**/*.{html,js}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('@tailwindcss/typography'), require('daisyui')],
+    daisyui: {
+        themes: ['night', 'light'],
+    },
+};
