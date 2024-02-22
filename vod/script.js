d6e7b0516ff170f1f36b65a34b55578e4c7bad0b
AlexFreik <alexfreik5@gmail.com>
2024-02-22 13:57:28 +0530

    prettier

--- vod/script.js
+++
@@ -1,7 +0,0 @@
-function stylePlayer() {
-    const iframe = document.getElementById('player').contentWindow.document;
-    const link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://alexfreik.github.io/lsm/vod/player-styles.css';
-    iframe.head.appendChild(link);
-}
