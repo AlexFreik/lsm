7aea92bfc9e9f4bfc67e07664f48489cbb403aa3
AlexFreik <alexfreik5@gmail.com>
2024-04-07 14:00:53 +0530

    zoom sdk init

---
+++ zoom-sdk/component-view.js
@@ -0,0 +1,63 @@
+const client = ZoomMtgEmbedded.createClient();
+
+let meetingSDKElement = document.getElementById('meetingSDKElement');
+
+var authEndpoint = '';
+var sdkKey = '';
+var meetingNumber = '123456789';
+var passWord = '';
+var role = 0;
+var userName = 'JavaScript';
+var userEmail = '';
+var registrantToken = '';
+var zakToken = '';
+
+function getSignature() {
+    fetch(authEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            meetingNumber: meetingNumber,
+            role: role,
+        }),
+    })
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            startMeeting(data.signature);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+function startMeeting(signature) {
+    client
+        .init({ zoomAppRoot: meetingSDKElement, language: 'en-US', patchJsMedia: true })
+        .then(() => {
+            client
+                .join({
+                    signature: signature,
+                    sdkKey: sdkKey,
+                    meetingNumber: meetingNumber,
+                    password: passWord,
+                    userName: userName,
+                    userEmail: userEmail,
+                    tk: registrantToken,
+                    zak: zakToken,
+                })
+                .then(() => {
+                    console.log('joined successfully');
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
