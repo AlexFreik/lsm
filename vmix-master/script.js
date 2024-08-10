383096e91e69cf5513358d89d74dddd364a12a0c
AlexFreik <alexfreik5@gmail.com>
2024-08-10 23:01:08 +0530

    init execute logic

--- vmix-master/script.js
+++ vmix-master/script.js
@@ -1,4 +1,4 @@
-import { getBoxUrlParams } from './tools.js';
+import { getBoxUrlParams, executeAndShow, getConfigUrlParams, updateUrlParams } from './tools.js';
 import { createBox, createSwapBtn } from './box.js';
 
 function addBox(name = '', host = '') {
@@ -9,18 +9,51 @@ function addBox(name = '', host = '') {
     boxes.insertBefore(createBox(name, host), boxes.lastElementChild);
 }
 
+function initConfig() {
+    const urlParams = getConfigUrlParams();
+    urlParams.forEach((param) => {
+        const input = document.getElementById(param.key);
+        console.assert(input);
+        if (input.type === 'checkbox') {
+            console.assert(['0', '1'].includes(param.value));
+            input.checked = param.value === '1';
+        } else if (input.type === 'text') {
+            input.value = param.value;
+        } else {
+            console.error('Unknown input type: ' + input.type);
+        }
+    });
+}
+
 function initBoxes() {
     const urlParams = getBoxUrlParams();
     if (urlParams.length === 0) {
         addBox('', '');
     }
     urlParams.forEach((param) => {
-        addBox(param.key, param.value.substring(0, 2), param.value.substring(2));
+        addBox(param.key, param.value);
+    });
+}
+
+function executeRawRequest() {
+    const request = document.getElementById('rawRequest').value;
+    const boxes = getBoxUrlParams();
+    boxes.forEach((vmix) => {
+        executeAndShow('http://' + vmix.value + ':8088/api/?' + request);
     });
 }
 
 (() => {
+    initConfig();
     initBoxes();
+
+    document
+        .querySelectorAll('.url-param')
+        .forEach((input) => input.addEventListener('change', updateUrlParams));
+
     const addBtn = document.getElementById('add-box');
     addBtn.addEventListener('click', () => addBox());
+
+    const executeBtn = document.getElementById('execute-btn');
+    executeBtn.onclick = executeRawRequest;
 })();
--- vmix-master/tools.js
+++ vmix-master/tools.js
@@ -8,11 +8,37 @@ function getHost(box) {
     return box.getElementsByClassName('host-input')[0].value;
 }
 
-function getBoxUrlParams() {
+function parseDocumentConfig() {
+    const params = new URLSearchParams();
+
+    document.querySelectorAll('.url-param').forEach((input) => {
+        if (input.type === 'checkbox') {
+            params.append('__' + input.id, input.checked ? '1' : '0');
+        } else if (input.type === 'text') {
+            params.append('__' + input.id, input.value);
+        } else {
+            conole.error('unexpected type: ' + input.type);
+        }
+    });
+    return params;
+}
+
+function getConfigUrlParams() {
     const url = window.location.href;
     const searchParams = new URLSearchParams(new URL(url).search);
     const params = [];
     searchParams.forEach(function (value, key) {
+        if (key === '' || !key.startsWith('__')) return;
+        params.push({ key: key.substring(2), value: value });
+    });
+    return params;
+}
+
+function getBoxUrlParams() {
+    const url = window.location.href;
+    const searchParams = new URLSearchParams(new URL(url).search);
+    const params = [];
+    searchParams.forEach((value, key) => {
         if (key === '' || key.startsWith('__')) return;
         params.push({ key: key, value: value });
     });
@@ -27,11 +53,80 @@ function updateUrlParams() {
         if (key === '') return;
         boxParams.append(key, val);
     });
-    window.history.pushState({}, '', `?${boxParams.toString()}`);
+    const configParams = parseDocumentConfig();
+    window.history.pushState({}, '', `?${boxParams.toString()}&${configParams.toString()}`);
 }
 
-function capitalizeFirst(str) {
-    return str.charAt(0).toUpperCase() + str.slice(1);
+function show(str, isError = false) {
+    const logs = document.getElementById('logs');
+    logs.innerHTML =
+        `
+        <p ${isError ? 'class="text-error"' : ''}>
+            ${str}
+        </p>
+        <div class="divider"></div>` + logs.innerHTML;
 }
 
-export { getBoxUrlParams, updateUrlParams, capitalizeFirst };
+function showError(str) {
+    show(str, true);
+}
+
+async function executeAndShow(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.text();
+
+        const timestamp = new Date().toLocaleTimeString();
+        const message = `[${timestamp}] ${url} Status ${response.status}. ${data.slice(0, 300)}`;
+        if (response.ok) {
+            show(message);
+        } else {
+            showError(message);
+        }
+    } catch (error) {
+        const timestamp = new Date().toLocaleTimeString();
+        showError(`[${timestamp}] Error ${url}`, error);
+    }
+    return [];
+}
+
+function xml2json(xml) {
+    // Create the return object
+    var obj = {};
+
+    if (xml.nodeType == 1) {
+        // element
+        // do attributes
+        if (xml.attributes.length > 0) {
+            obj['@attributes'] = {};
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj['@attributes'][attribute.nodeName] = attribute.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3) {
+        // text
+        obj = xml.nodeValue;
+    }
+
+    // do children
+    if (xml.hasChildNodes()) {
+        for (var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof obj[nodeName] == 'undefined') {
+                obj[nodeName] = xml2json(item);
+            } else {
+                if (typeof obj[nodeName].push == 'undefined') {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xml2json(item));
+            }
+        }
+    }
+    return obj;
+}
+
+export { getConfigUrlParams, getBoxUrlParams, updateUrlParams, executeAndShow, xml2json };
