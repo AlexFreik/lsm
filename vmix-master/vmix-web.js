89e5039e479d24d5535c214f3d680767495746d5
AlexFreik <alexfreik5@gmail.com>
2024-10-23 12:44:21 +0530

    bug fix, added audio mixer

--- vmix-master/vmix-web.js
+++ vmix-master/vmix-web.js
@@ -3,34 +3,34 @@ import { getBox } from './box.js';
 import { getVmixInfo } from './vmix-info.js';
 
 async function renderVmixWeb() {
-    const vmixButtons = document.getElementById('vmixButtons');
+    const vmixContainer = document.getElementById('vmixContainer');
 
     const master = getMaster();
     if (master === null) {
-        vmixButtons.innerHTML = '';
+        vmixContainer.innerHTML = '';
         return;
     }
     const box = getBox(master);
     const vmixInfo = await getVmixInfo(getBoxHost(box) + ':8088');
 
-    let innerHTML = '';
     if (vmixInfo.error) {
-        innerHTML = vmixInfo.error;
-    } else {
-        const info = vmixInfo.value;
+        vmixContainer.innerHTML = '';
+        return;
+    }
+    const info = vmixInfo.value;
 
-        const activeInput = info.inputs[info.active];
-        const previewInput = info.inputs[info.preview];
-        innerHTML += `
+    const active = info.inputs[info.active];
+    const preview = info.inputs[info.preview];
+    const screensHTML = `
       <div class="grid text-center grid-rows-5 grid-cols-[1fr_120px_1fr] gap-1 w-[600px] h-[200px] mx-auto mt-5">
           <div class="row-span-4 col-span-1 bg-yellow-600 text-lg">
-              ${previewInput.title}
+              ${preview.title}
           </div>
           <div class="row-span-1 col-span-1">
               <button id="stinger1" class="btn btn-sm btn-neutral w-24">Stinger 1</button>
           </div>
           <div class="row-span-4 col-span-1 bg-green-700 text-lg">
-              ${activeInput.title}
+              ${active.title}
           </div>
           <div class="row-span-1 col-span-1">
               <button id="fade" class="btn btn-sm btn-neutral w-24">Fade</button>
@@ -41,46 +41,91 @@ async function renderVmixWeb() {
           <div class="row-span-1 col-span-1">
           </div>
           <div class="row-span-1 col-span-1">
-            ${previewInput.duration !== '0' ? getVideoProgress(previewInput) : ''}
+            ${preview.duration !== '0' ? getVideoProgress(preview) : ''}
           </div>
           <div class="row-span-1 col-span-1"></div>
           <div class="row-span-1 col-span-1">
-            ${activeInput.duration !== '0' ? getVideoProgress(activeInput) : ''}
+            ${active.duration !== '0' ? getVideoProgress(active) : ''}
           </div>
       </div>`;
 
-        info.inputs.forEach((input, i) => {
-            const isActive = i === info.active;
-            const isPreview = i === info.preview;
-            const style = isActive ? 'bg-green-700' : isPreview ? 'bg-yellow-600' : 'bg-neutral';
-            console.lo;
-            innerHTML += `
-                <div class="inline-block mx-1 my-1 border border-neutral">
-                    <div class="vmixInput ${style} w-64 cursor-pointer" data-number="${i}">
-                        <span class="badge badge-neutral mx-1 my-1">${input.number}</span>
-                        ${input.title.length > 20 ? input.title.slice(0, 20) + '...' : input.title}
-                    </div>
-                    <div class="m-1">
-                    <span class="badge ${info.overlays[1] === i ? 'bg-green-700' : 'badge-neutral'}">1</span>
-                    <span class="badge ${info.overlays[2] === i ? 'bg-green-700' : 'badge-neutral'}">2</span>
-                    <span class="badge ${info.overlays[3] === i ? 'bg-green-700' : 'badge-neutral'}">3</span>
-                    <span class="badge ${info.overlays[4] === i ? 'bg-green-700' : 'badge-neutral'}">4</span>
-                    <span class="badge ${input.muted === 'False' ? 'bg-green-700' : 'badge-neutral'}">AUDIO</span>
-                    </div>
+    let inputsHTML = '<div>';
+    let mixerHTML = '<div class="mt-5">';
+    info.inputs.forEach((input, i) => {
+        const isActive = i === info.active;
+        const isPreview = i === info.preview;
+        const style = isActive ? 'bg-green-700' : isPreview ? 'bg-yellow-600' : 'bg-neutral';
+        inputsHTML += `
+            <div class="inline-block mx-1 my-1 border border-neutral">
+                <div class="vmixInput ${style} w-64 cursor-pointer" data-number="${i}">
+                    <span class="badge badge-neutral mx-1 my-1">${input.number}</span>
+                    ${input.title.length > 20 ? input.title.slice(0, 20) + '...' : input.title}
                 </div>
-            `;
-        });
-    }
-    vmixButtons.innerHTML = innerHTML;
+                <div class="m-1">
+                <span class="badge ${info.overlays[1] === i ? 'bg-green-700' : 'badge-neutral'}">1</span>
+                <span class="badge ${info.overlays[2] === i ? 'bg-green-700' : 'badge-neutral'}">2</span>
+                <span class="badge ${info.overlays[3] === i ? 'bg-green-700' : 'badge-neutral'}">3</span>
+                <span class="badge ${info.overlays[4] === i ? 'bg-green-700' : 'badge-neutral'}">4</span>
+                <span class="badge ${input.muted === 'False' ? 'bg-green-700' : 'badge-neutral'}">AUDIO</span>
+                </div>
+            </div>`;
+        if (input.volume !== undefined) {
+            const meterF1 = Math.round(parseFloat(input.meterF1) * 100);
+            const meterF2 = Math.round(parseFloat(input.meterF2) * 100);
+            mixerHTML += `
+              <div class="inline-block w-24 border border-neutral pb-1 m-1 bg-base-100">
+                <div class="${input.muted === 'False' ? 'bg-green-700' : 'bg-primary-content'} mb-1">
+                  <span class="badge badge-neutral mx-1 my-1">${input.number}</span>
+                  ${input.title.slice(0, 5)}
+                </div>
+                <div class="relative ">
+                  <div class="inline-block h-24 w-1">
+                      <div class="bg-black" style="height: ${100 - meterF1}%"></div>
+                      <div class="bg-green-500" style="height: ${meterF1}%"></div>
+                  </div>
+                  <div class="inline-block h-24 w-1">
+                      <div class="bg-black" style="height: ${100 - meterF2}%"></div>
+                      <div class="bg-green-500" style="height: ${meterF2}%"></div>
+                  </div>
+                  <div class="inline-block -mt-5 text-center mx-2">
+                    ${Math.round(input.volume)}%
+                    <br />
+                    <button class="fadeAudioOut btn btn-xs btn-outline" data-number="${i}">0%</button>
+                    <br />
+                    <button class="fadeAudioIn btn btn-xs btn-outline" data-number="${i}">100%</button>
+                    <br />
+                    <span>&nbsp;</span>
+                  </div>
+                </div>
+                <div class="px-1">
+                    <span class="badge badge-sm ${input.audiobusses.includes('M') ? 'bg-green-700' : 'badge-neutral'}">M</span>
+                    <span class="badge badge-sm ${input.audiobusses.includes('A') ? 'bg-green-700' : 'badge-neutral'}">A</span>
+                    <span class="badge badge-sm ${input.audiobusses.includes('B') ? 'bg-green-700' : 'badge-neutral'}">B</span>
+                </div>
+              </div>`;
+        }
+    });
+    inputsHTML += '</div>';
+    mixerHTML += '</div>';
+
+    vmixContainer.innerHTML = screensHTML + inputsHTML + mixerHTML;
 
     document
         .querySelectorAll('.vmixInput')
         .forEach((elem) => (elem.onclick = () => previewInput(elem.getAttribute('data-number'))));
+    document
+        .querySelectorAll('.fadeAudioOut')
+        .forEach((elem) => (elem.onclick = () => fadeAudioOut(elem.getAttribute('data-number'))));
+    document
+        .querySelectorAll('.fadeAudioIn')
+        .forEach((elem) => (elem.onclick = () => fadeAudioIn(elem.getAttribute('data-number'))));
 
-    document.getElementById('stinger1').onclick = () => transition('Stinger1');
-    document.getElementById('fade').onclick = () => transition('Fade');
-    document.getElementById('cut').onclick = () => transition('Cut');
+    document.getElementById('stinger1').onclick = () => transition('Stinger1', preview.number);
+    document.getElementById('fade').onclick = () => transition('Fade', preview.number);
+    document.getElementById('cut').onclick = () => transition('Cut', preview.number);
 }
+
+function getMixer() {}
 
 function getMaster() {
     const master = parseInt(document.getElementById('master').value);
@@ -119,8 +164,16 @@ function previewInput(inputNum) {
     masterSlaveExecute('Function=PreviewInput&Input=' + inputNum);
 }
 
-function transition(type) {
-    masterSlaveExecute('Function=' + type);
+function transition(type, inputNum) {
+    masterSlaveExecute('Function=' + type + '&Input=' + inputNum);
+}
+
+function fadeAudioIn(inputNum) {
+    masterSlaveExecute('Function=SetVolumeFade&Value=100,3000&Input=' + inputNum);
+}
+
+function fadeAudioOut(inputNum) {
+    masterSlaveExecute('Function=SetVolumeFade&Value=0,3000&Input=' + inputNum);
 }
 
 function masterSlaveExecute(command) {
