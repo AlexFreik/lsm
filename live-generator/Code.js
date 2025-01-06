75583c41c51f0766f03f572bbe400012ac7dee70
AlexFreik <alexfreik5@gmail.com>
2025-01-06 21:04:34 +0530

    improved live generator, mvp

--- live-generator/Code.js
+++ live-generator/Code.js
@@ -2,104 +2,73 @@ const props = PropertiesService.getScriptProperties();
 
 const CLIENT_ID = props.getProperty('CLIENT_ID');
 const CLIENT_SECRET = props.getProperty('CLIENT_SECRET');
+const SPREADSHEET_ID = props.getProperty('SPREADSHEET_ID');
+const KEY_SHEET_NAME = 'Keys';
+const SCHEDULE_SHEET_NAME = 'Schedule';
 
-/**
- * Adds a custom menu with items to show the sidebar.
- * @param {Object} e The event parameter for a simple onOpen trigger.
- */
+// ===== Sidebar =====
 function onOpen(e) {
-    SpreadsheetApp.getUi()
-        .createAddonMenu()
-        .addItem('Show Sidebar', 'showSidebar')
-        .addItem('Sign Out', 'signOut')
-        .addItem('Add Selected Rows', 'addSelectedRows')
-        .addItem('Log Token', 'logToken')
-        .addToUi();
+    SpreadsheetApp.getUi().createAddonMenu().addItem('Show Sidebar', 'showSidebar').addToUi();
 }
 
 function showSidebar() {
     const service = getYouTubeService_();
+    const template = HtmlService.createTemplateFromFile('Sidebar');
+    template.authorizationUrl = service.getAuthorizationUrl();
 
-    if (!service.hasAccess()) {
-        const authorizationUrl = service.getAuthorizationUrl();
-        const template = HtmlService.createTemplate(
-            '<a href="<?= authorizationUrl ?>" target="_blank">Authorize</a>. ' +
-                'Reopen the sidebar when the authorization is complete.',
-        );
-        template.authorizationUrl = authorizationUrl;
-        const page = template.evaluate();
-        SpreadsheetApp.getUi().showSidebar(page);
-    } else {
-        const template = HtmlService.createTemplate(
-            'You are signed in as <?= email ?> and token is <?= token ?>',
-        );
-        template.email = Session.getEffectiveUser().getEmail();
-        template.token = service.getAccessToken();
-        const page = template.evaluate();
-        SpreadsheetApp.getUi().showSidebar(page);
+    const page = template.evaluate().setTitle('Live Generator');
+    SpreadsheetApp.getUi().showSidebar(page);
+}
+
+function getUserEmail() {
+    return Session.getEffectiveUser().getEmail();
+}
+
+// ===== Spreadsheet Logic =====
+function getColumnIndex(sheet, columnName) {
+    const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
+    return headers.indexOf(columnName) + 1; // Add 1 because indexOf() is 0-based
+}
+
+function scheduleBroadcasts() {
+    const rows = getSelectedRows();
+
+    const sheet = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName(SCHEDULE_SHEET_NAME);
+    const broadcastIdColumn = getColumnIndex(sheet, 'Broadcast ID');
+    for (let row of rows) {
+        try {
+            const broadcast = insertBroadcast(row);
+            bindBroadcast(broadcast.id, row['Key ID']);
+
+            const broadcastIdCell = sheet.getRange(row.row, broadcastIdColumn);
+            broadcastIdCell.setValue(broadcast.id);
+            broadcastIdCell.setBackground('#fce5cd');
+        } catch (e) {
+            const broadcastIdCell = sheet.getRange(row.row, broadcastIdColumn);
+            broadcastIdCell.setBackground('red');
+            throw e;
+        }
     }
+    return `Successfully added ${rows.length} broadcasts.`;
 }
 
-function authCallback(request) {
-    const service = getYouTubeService_();
-    const isAuthorized = service.handleCallback(request);
-    if (isAuthorized) {
-        return HtmlService.createHtmlOutput('Success! You can close this tab.');
-    } else {
-        return HtmlService.createHtmlOutput('Denied. You can close this tab.');
+function updateStreamKeys() {
+    const spreadsheet = SpreadsheetApp.openById(SPREADSHEET_ID);
+    const sheet = spreadsheet.getSheetByName(KEY_SHEET_NAME);
+    const data = getStreams().map((item) => [item.id, item.title, item.key]);
+
+    const currentRows = sheet.getLastRow();
+    if (currentRows > 2) {
+        sheet.deleteRows(2, currentRows - 1);
     }
+    if (data.length > 0) {
+        sheet.getRange(2, 1, data.length, data[0].length).setValues(data);
+    }
+
+    return `Successfully refreshed ${data.length} keys`;
 }
 
-function getToken_() {
-    return getYouTubeService_().getAccessToken();
-}
-
-/**
- * Resets the API service, forcing re-authorization before
- * additional authorization-required API calls can be made.
- */
-function signOut() {
-    getYouTubeService_().reset();
-}
-
-/**
- * Gets an OAuth2 service configured for the GitHub API.
- * @return {OAuth2.Service} The OAuth2 service
- */
-function getYouTubeService_() {
-    // Create a new service with the given name. The name will be used when
-    // persisting the authorized token, so ensure it is unique within the
-    // scope of the property store.
-    return (
-        OAuth2.createService('youtube')
-
-            // Set the endpoint URLs, which are the same for all Google services.
-            .setAuthorizationBaseUrl('https://accounts.google.com/o/oauth2/auth')
-            .setTokenUrl('https://accounts.google.com/o/oauth2/token')
-
-            // Set the client ID and secret, from the Google Developers Console.
-            .setClientId(CLIENT_ID)
-            .setClientSecret(CLIENT_SECRET)
-
-            // Set the name of the callback function in the script referenced
-            // above that should be invoked to complete the OAuth flow.
-            .setCallbackFunction('authCallback')
-
-            // Set the property store where authorized tokens should be persisted.
-            .setPropertyStore(PropertiesService.getUserProperties())
-
-            // Set the scopes to request (space-separated for Google services).
-            .setScope([
-                'https://www.googleapis.com/auth/youtube',
-                'https://www.googleapis.com/auth/youtube.force-ssl',
-                'https://www.googleapis.com/auth/youtube.readonly',
-                'https://www.googleapis.com/auth/youtubepartner',
-                'https://www.googleapis.com/auth/youtubepartner-channel-audit',
-            ])
-    );
-}
-
-function getSelectedRowsData() {
+function getSelectedRows() {
     const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
     const range = sheet.getActiveRange(); // Get the selected range
     const selectedRows = range.getRow(); // First row of the selection
@@ -109,35 +78,75 @@ function getSelectedRowsData() {
     const data = [];
 
     for (let row = selectedRows; row <= lastSelectedRow; row++) {
-        // Skip the header row (assumed to be the first row)
         if (row === 1) continue;
-
         const rowData = sheet.getRange(row, 1, 1, numCols).getValues()[0];
-
-        // Skip empty rows
         if (rowData.every((cell) => cell === '')) continue;
-
-        // Create an object with header keys and row values
-        const rowObject = {};
+        const rowObject = { row: row };
         headers.forEach((header, colIndex) => {
-            rowObject[header] = rowData[colIndex];
+            rowObject[header] = String(rowData[colIndex]).trim();
         });
-
         data.push(rowObject);
     }
 
-    console.log(JSON.stringify(data, null, 2)); // Log the result for debugging
-    return data; // Return the array of row objects
+    formatBroadcastData(data);
+    console.log(JSON.stringify(data, null, 2));
+    return data;
 }
 
-function addSelectedRows() {
-    const rows = getSelectedRowsData();
-    rows.forEach((row) => {
-        const broadcast = insertBroadcast(row);
-        console.log(broadcast);
-        const stream = insertStream(row);
-        console.log(stream);
-        const bind = bindBroadcast(broadcast.id, stream.id);
-        console.log(bind);
-    });
+// ===== Validations & Formating =====
+function isNonNegativeInteger(str) {
+    return /^\d+$/.test(str);
+}
+
+function validateNumber(str, min, max) {
+    if (str === '') {
+        return true;
+    }
+    if (!isNonNegativeInteger(str)) return false;
+
+    const num = parseInt(str);
+    return min <= num && num <= max;
+}
+
+function getValidationError(row, msg) {
+    return new Error(`[Invalid row #${row}] ${msg}.`);
+}
+
+function pad(str, length = 2) {
+    return String(Number(str)).padStart(length, '0');
+}
+
+function formatBroadcastData(data) {
+    const now = new Date(Date.now() + 2 * (60 * 60 * 1000));
+
+    for (let row of data) {
+        if (row.Title === '') {
+            throw getValidationError(row.row, "Title can't be empty");
+        } else if (!validateNumber(row.Year, 2000, 2100)) {
+            throw getValidationError(row.row, 'Year value shold be between 2000 and 2100');
+        } else if (!validateNumber(row.Month, 1, 12)) {
+            throw getValidationError(row.row, 'Month value shold be between 1 and 12');
+        } else if (!validateNumber(row.Day, 1, 31)) {
+            throw getValidationError(row.row, 'Day value shold be between 1 and 31');
+        } else if (!validateNumber(row.Hour, 0, 23)) {
+            throw getValidationError(row.row, 'Hour value shold be between 0 and 23');
+        } else if (!validateNumber(row.Minute, 0, 59)) {
+            throw getValidationError(row.row, 'Minute value shold be between 0 and 59');
+        } else if (row.Visibility === '') {
+            throw getValidationError(row.row, 'Visibility is not defined');
+        } else if (row['Key ID'] === '') {
+            throw getValidationError(row.row, 'Key ID is not defined');
+        } else if (row['Broadcast ID'] !== '') {
+            throw getValidationError(row.row, 'Broadcast ID is alfready set');
+        }
+
+        row.Year = pad(row.Year === '' ? now.getFullYear() : row.Year, 4);
+        row.Month = pad(row.Month === '' ? now.getMonth() + 1 : row.Month);
+        row.Day = pad(row.Day === '' ? now.getDate() : row.Day);
+        row.Hour = pad(row.Hour === '' ? now.getHours() : row.Hour);
+        row.Minute = pad(row.Minute === '' ? 0 : row.Minute);
+        row.DVR = Boolean(row.DVR);
+        row['Auto Start'] = Boolean(row['Auto Start']);
+        row['Auto Stop'] = Boolean(row['Auto Stop']);
+    }
 }
---
+++ live-generator/YouTube.js
@@ -0,0 +1,194 @@
+// ===== Auth API =====
+function getYouTubeService_() {
+    // Create a new service with the given name. The name will be used when
+    // persisting the authorized token, so ensure it is unique within the
+    // scope of the property store.
+    return (
+        OAuth2.createService('youtube')
+
+            // Set the endpoint URLs, which are the same for all Google services.
+            .setAuthorizationBaseUrl('https://accounts.google.com/o/oauth2/auth')
+            .setTokenUrl('https://accounts.google.com/o/oauth2/token')
+
+            // Set the client ID and secret, from the Google Developers Console.
+            .setClientId(CLIENT_ID)
+            .setClientSecret(CLIENT_SECRET)
+
+            // Set the name of the callback function in the script referenced
+            // above that should be invoked to complete the OAuth flow.
+            .setCallbackFunction('authCallback')
+
+            // Set the property store where authorized tokens should be persisted.
+            .setPropertyStore(PropertiesService.getUserProperties())
+
+            // Set the scopes to request (space-separated for Google services).
+            .setScope([
+                'https://www.googleapis.com/auth/youtube',
+                'https://www.googleapis.com/auth/youtube.force-ssl',
+                'https://www.googleapis.com/auth/youtube.readonly',
+                'https://www.googleapis.com/auth/youtubepartner',
+                'https://www.googleapis.com/auth/youtubepartner-channel-audit',
+            ])
+    );
+}
+
+function authCallback(request) {
+    const service = getYouTubeService_();
+    const isAuthorized = service.handleCallback(request);
+    if (isAuthorized) {
+        showSidebar();
+        return HtmlService.createHtmlOutput('Success! You can close this tab.');
+    } else {
+        return HtmlService.createHtmlOutput('Denied. You can close this tab.');
+    }
+}
+
+function hasAccess() {
+    return getYouTubeService_().hasAccess();
+}
+
+function getToken_() {
+    if (!hasAccess()) {
+        showSidebar();
+        throw new Error('Please Sign In, your token has expired.');
+    }
+    return getYouTubeService_().getAccessToken();
+}
+
+function signOut() {
+    getYouTubeService_().reset();
+}
+
+// ===== YouTube API =====
+const BASEURL = 'https://www.googleapis.com/youtube/v3/';
+
+function buildUrl(url, params) {
+    var params = params || {}; //allow for NULL options
+    var paramString = Object.keys(params)
+        .map(function (key) {
+            return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+        })
+        .join('&');
+    return url + (url.indexOf('?') >= 0 ? '&' : '?') + paramString;
+}
+
+function fetchUrl(path, token, options = {}) {
+    const fetchOptions = {
+        method: '',
+        muteHttpExceptions: true,
+        contentType: 'application/json',
+        headers: { Authorization: 'Bearer ' + token },
+    };
+    const url = BASEURL + path;
+
+    for (option in options) {
+        fetchOptions[option] = options[option];
+    }
+
+    const response = UrlFetchApp.fetch(url, fetchOptions);
+    if (response.getResponseCode() != 200) {
+        throw new Error(response.getContentText());
+    } else {
+        return JSON.parse(response.getContentText());
+    }
+}
+
+function fetchPage(path, token, options, returnParamPath) {
+    var fetchOptions = {
+        method: '',
+        muteHttpExceptions: true,
+        contentType: 'application/json',
+        headers: { Authorization: 'Bearer ' + token },
+    };
+    for (option in options) {
+        fetchOptions[option] = options[option];
+    }
+
+    const ret = [];
+    let nextPageToken;
+    let url = BASEURL + path;
+    do {
+        const response = UrlFetchApp.fetch(url, fetchOptions);
+
+        if (response.getResponseCode() != 200) {
+            throw new Error(response.getContentText());
+        } else {
+            const data = JSON.parse(response.getContentText());
+            console.log(data.items.map((item) => item.snippet.title).join('; '));
+            nextPageToken = data.nextPageToken;
+            ret.push(...data[returnParamPath]);
+        }
+        url = buildUrl(BASEURL + path, { pageToken: nextPageToken });
+    } while (nextPageToken);
+    return ret;
+}
+
+function insertBroadcast(data) {
+    const start = `${data.Year}-${data.Month}-${data.Day}T${data.Hour}:${data.Minute}:00+0530`;
+    const payload = {
+        snippet: {
+            title: data.Title,
+            description: '',
+            scheduledStartTime: start,
+            scheduledEndTime: start,
+        },
+        status: {
+            privacyStatus: data.Visibility,
+        },
+        contentDetails: {
+            enableDvr: data.DVR,
+            enableAutoStart: data['Auto Start'],
+            enableAutoStop: data['Auto Stop'],
+        },
+    };
+    const path = buildUrl('liveBroadcasts', { part: 'snippet,contentDetails,status' });
+    const callOptions = { method: 'POST', payload: JSON.stringify(payload) };
+    const response = fetchUrl(path, getToken_(), callOptions);
+    console.log(response);
+    return response;
+}
+
+function insertStream(data) {
+    const payload = {
+        snippet: {
+            title: data.Title,
+        },
+        cdn: {
+            ingestionType: 'rtmp',
+            frameRate: '60fps',
+            resolution: '1080p',
+        },
+    };
+    const path = buildUrl('liveStreams', { part: 'snippet,cdn' });
+    const callOptions = { method: 'POST', payload: JSON.stringify(payload) };
+    const response = fetchUrl(path, getToken_(), callOptions);
+    console.log(response);
+    return response;
+}
+
+// Bind the broadcast to the video stream. By doing so, you link the video that
+// you will transmit to YouTube to the broadcast that the video is for.
+function bindBroadcast(broadcastId, streamId) {
+    const path = buildUrl('liveBroadcasts/bind', {
+        part: 'id,contentDetails',
+        id: broadcastId,
+        streamId: streamId,
+    });
+    const callOptions = { method: 'POST' };
+    const response = fetchUrl(path, getToken_(), callOptions);
+    console.log(response);
+    return response;
+}
+
+function getStreams() {
+    const path = buildUrl('liveStreams', { part: 'snippet,cdn', mine: 'true' });
+    const callOptions = { method: 'GET' };
+    const response = fetchPage(path, getToken_(), callOptions, 'items');
+    const streams = response.map((item) => ({
+        id: item.id,
+        title: item.snippet.title,
+        key: item.cdn.ingestionInfo.streamName,
+    }));
+    console.log(streams);
+    return streams;
+}
